f440015b6e6ec9e75e5aad9eb3947d8d709b379b
Blitz <blitz04.dev@gmail.com>
2022-11-12 20:42:40 +0530

    profile management

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder="./static")
 
 @app.route('/')
 def _():
-    return render_template('index.html')
+    return render_template('home.html')
 
 
 @app.route('/scan')
