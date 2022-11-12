9af0dc7fac7213b265a532baae5b33fba74a2385
Joshua Bharathi <blitz04.dev@gmail.com>
2022-11-12 07:42:58 +0000

    scanning page complete

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_folder="./static")
 
 
-app = Flask(__name__)
+@app.route('/')
+def _():
+    return render_template('index.html')
 
 
 @app.route('/scan')
@@ -12,6 +16,17 @@ def index_():
 @app.route('/enter_amount')
 def enter_amount_():
     return render_template('enter_amount.html')
+
+
+@app.route('/sw.js')
+def __sw():
+    return send_from_directory('.', 'sw.js')
+
+
+@app.route('/manifest.json')
+def __manifest():
+    return send_from_directory('.', 'manifest.json')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
